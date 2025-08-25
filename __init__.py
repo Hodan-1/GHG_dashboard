f29e6bb429112d3569532033bcd389f50a55bd01
Hodan-1 <hodanabdi24@outlook.com>
2025-08-25 14:10:26 +0100

    ' adding updated dat files and init.py'

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+GHG Emissions Dashboard Package
+A comprehensive Streamlit dashboard for analyzing greenhouse gas emissions data.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Hodan Abdi"
+__description__ = "Interactive dashboard for GHG emissions analysis and visualization"
